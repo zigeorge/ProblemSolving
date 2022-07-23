186f90ef045d8682776ead157dab636bf1045e21
George <zahidul@sheba.xyz>
2022-07-23 22:36:37 +0600

    hudai kisu kaj korsi

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,15 +1,26 @@
+import static org.junit.Assert.assertArrayEquals;
 import static org.junit.Assert.assertEquals;
+
+
+import java.util.LinkedList;
+import java.util.Queue;
 
 import org.junit.Test;
 
 public class BinaryTree {
-
-
     @Test
     public void testTreeNodeToString() {
-        TreeNode root = TreeNode.creatTreeNode(new int[] {1,2,3,4,5});
+        TreeNode root = TreeNode.creatTreeNode(new Integer[] {1,2,3,4,5});
         String result = TreeNode.toString(root);
-        assertEquals("12345", result);
+        assertEquals("12453", result);
+    }
+
+    @Test
+    public void testTreeNodeToArray() {
+        TreeNode root = TreeNode.creatTreeNode(new Integer[] {1,2,3,4,5,6,7});
+        int[] result = TreeNode.toArray(root);
+        // Arrays.sort(result);
+        assertArrayEquals(new int[]{1,2,4,5,3,6,7}, result);
     }
 }
 
@@ -28,8 +39,8 @@ class TreeNode {
     private static String result = "";
     private static void createString(TreeNode root) {
         if(root != null) {
-            createString(root.left);
             result = result.concat(String.valueOf(root.val));
+            createString(root.left);
             createString(root.right);
         }
     }
@@ -39,14 +50,32 @@ class TreeNode {
         return result;
     }
 
-    public static TreeNode creatTreeNode(int[] arr) {
+    public static TreeNode creatTreeNode(Integer[] arr) {
         return fromArray(arr, 0);
     }
 
-    private static TreeNode fromArray(int[] arr, int i) {
+    private static Queue<Integer> qu = new LinkedList<>();
+    public static int[] toArray(TreeNode root) {
+        createArray(root);
+        int[] arr = new int[qu.size()];
+        for(int i = 0; i<arr.length; i++) {
+            arr[i] = qu.poll();
+        }
+        return arr;
+    }
+
+    private static void createArray(TreeNode root) {
+        if(root != null) {
+            qu.add(root.val);
+            createArray(root.left);
+            createArray(root.right);
+        }
+    }
+
+    private static TreeNode fromArray(Integer[] arr, int i) {
         TreeNode root = null;
         // Base case for recursion
-        if (i < arr.length) {
+        if (i < arr.length && arr[i] != null) {
             root = new TreeNode(arr[i]);
   
             // insert left child
